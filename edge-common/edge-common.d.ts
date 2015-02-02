af15b1af45d56c3eadec6a198c2bf45e4ac4baea
wokim <wonshikkim.kr@gmail.com>
2015-02-03 00:33:57 +0900

    Added interfaces which are used in both server and client.

--- edge-common/edge-common.d.ts
+++ edge-common/edge-common.d.ts
@@ -23,4 +23,42 @@ declare module "edge-common" {
       rid: string;
     };
   }
+
+  // NOTE: done is better than perfect.
+  // 서브모듈을 용도에 맞게 잘 나눠보자.
+  export module Core {
+    interface IRacer {
+      rid: string;
+      name: string;
+      level: number;
+      carid: number;
+    }
+
+    interface IVehicle {
+      rid: string;
+      vid: number;
+      grade: number;
+      exp: number;
+    }
+  }
+
+  export module Match {
+    export enum GameState {
+      WAIT,
+      READY,
+      RACE
+    }
+
+    interface IGame {
+      id: number;
+      state: GameState;
+      entries: IEntry[];
+      createAt: number;
+    }
+
+    interface IEntry {
+      racer: Core.IRacer;
+      ready: boolean;
+    }
+  }
 }
