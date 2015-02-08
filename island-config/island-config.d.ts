20c44def111823105613613cae54baaad3cb1f1a
wonshikkim.kr@gmail.com <wonshikkim.kr@gmail.com>
2015-02-08 17:14:48 +0900

    Rename 'island-discovery' to 'island-config'

--- island-config/island-config.d.ts
+++ island-config/island-config.d.ts
@@ -1,13 +1,13 @@
-// Type definitions for island-discovery 0.0.1
-// Project: https://github.com/wokim/island-discovery
+// Type definitions for island-config 0.0.1
+// Project: https://github.com/wokim/island-config
 // Definitions by: Wonshik Kim <https://github.com/wokim/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../bluebird/bluebird.d.ts" />
 
-declare module "island-discovery" {
+declare module "island-config" {
   import Promise = require('bluebird');
-  export class IslandDiscovery {
+  export class IslandConfig {
     private etcd;
     constructor(host: string, port?: number);
     private parseNode(obj, node);
